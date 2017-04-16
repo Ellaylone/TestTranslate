7cf5698009b52dfdd87f5d6af2a5b98659290426
Ellaylone <ellaylone@yandex-team.ru>
2017-04-16 18:10:57 +0300

    bottom tab navigation

--- app/src/main/java/com/example/ellaylone/testtranslate/MainActivity.java
+++ app/src/main/java/com/example/ellaylone/testtranslate/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.ellaylone.testtranslate;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupBottomNavigation();
+    }
+
+    private void setupBottomNavigation() {
+        SwipeViewPager swipeViewPager = (SwipeViewPager) findViewById(R.id.pager);
+
+        CategoryAdapter categoryAdapter = new CategoryAdapter(this, getSupportFragmentManager());
+
+        swipeViewPager.setAdapter(categoryAdapter);
+        swipeViewPager.setEnabled(false);
+
+        final TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(swipeViewPager);
     }
 }
---
+++ app/src/main/java/com/example/ellaylone/testtranslate/SwipeViewPager.java
@@ -0,0 +1,39 @@
+package com.example.ellaylone.testtranslate;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * Created by ellaylone on 16.04.17.
+ */
+
+public class SwipeViewPager extends ViewPager {
+    private boolean enabled;
+
+    public SwipeViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        enabled = true;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        return enabled ? super.onTouchEvent(ev) : false;
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        return enabled ? super.onInterceptTouchEvent(ev) : false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    @Override
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+}
