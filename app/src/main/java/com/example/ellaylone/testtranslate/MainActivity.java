80a5bfcfa6082e447b8a49963997892dd56d35dd
Ellaylone <ellaylone@yandex-team.ru>
2017-04-19 14:09:55 +0300

    text area lose focus

--- app/src/main/java/com/example/ellaylone/testtranslate/MainActivity.java
+++ app/src/main/java/com/example/ellaylone/testtranslate/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.ellaylone.testtranslate;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.LinearLayout;
 
 import com.example.ellaylone.testtranslate.adapters.CategoryAdapter;
 
@@ -13,6 +18,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         setupBottomNavigation();
+
+        final LinearLayout root = (LinearLayout) findViewById(R.id.main_parent);
+        root.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                root.requestFocus();
+                InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(root.getWindowToken(), 0);
+                return false;
+            }
+        });
     }
 
     private void setupBottomNavigation() {
---
+++ app/src/main/java/com/example/ellaylone/testtranslate/TextArea.java
@@ -0,0 +1,45 @@
+package com.example.ellaylone.testtranslate;
+
+import android.content.Context;
+import android.support.v7.widget.AppCompatEditText;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by ellaylone on 18.04.17.
+ */
+
+public class TextArea extends AppCompatEditText implements View.OnFocusChangeListener {
+    AppCompatEditText textArea;
+
+    public TextArea(Context context) {
+        super(context);
+    }
+
+    public TextArea(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public TextArea(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+
+    @Override
+    public void onFocusChange(View v, boolean hasFocus) {
+        if (v.getId() == R.id.source_text_area) {
+            setInputFocusState(hasFocus);
+        }
+    }
+
+    private void setInputFocusState(boolean hasFocus) {
+        setFocusBorder(hasFocus);
+    }
+
+    private void setFocusBorder(boolean active) {
+        textArea.setBackgroundResource(active ?
+                R.drawable.text_area_border_focus :
+                R.drawable.text_area_border_unfocus);
+    }
+}
